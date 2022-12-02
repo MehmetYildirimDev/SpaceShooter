cb66de90d823c8208d645fbfc7eacf060f330728
Mehmet Yıldırım <mehmetyildirim.developer@gmail.com>
2022-12-02 14:23:27 +0300

    feat: create laser shooting system

--- core/src/com/my/spaceshooter/GameScreen.java
+++ core/src/com/my/spaceshooter/GameScreen.java
@@ -10,6 +10,9 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
+import java.util.LinkedList;
+import java.util.ListIterator;
+
 class GameScreen implements Screen {
 
     //screen
@@ -27,8 +30,7 @@ class GameScreen implements Screen {
             playerLaserTextureRegion, enemyLaserTextureRegion;
 
     //timing
-//    private  int backgroundOffset;//zamanla degisim icin. Cunku haraket edicek
-    private float[] backgroundOffsets = {0,0,0,0};
+    private float[] backgroundOffsets = {0,0,0,0};//zamanla degisim icin. Cunku haraket edicek
     private float backgroundMaxScrollingSpeed;
     //world parameters
     private final int WORLD_WIDTH = 72;
@@ -37,12 +39,17 @@ class GameScreen implements Screen {
     //game objects
     private Ship playerShip;
     private Ship enemyShip;
+    private LinkedList<Laser> playerLaserList;
+    private LinkedList<Laser> enemyLaserList;
+        //linkedList neden => kolayca silme islemi icin. Cunku gemiye carpinca kolayca silmememiz gerek
+
+
 
     GameScreen(){
 
         camera = new OrthographicCamera();//2d kameralar ortagrafik olur persfektif degil
         viewport = new StretchViewport(WORLD_WIDTH,WORLD_HEIGHT,camera);//esnek goruntu alani
-//set up the texture atlas
+        //set up the texture atlas
         textureAtlas = new TextureAtlas("images.atlas");
 
         //setting up the background
@@ -60,18 +67,27 @@ class GameScreen implements Screen {
         enemyShipTextureRegion = textureAtlas.findRegion("enemyRed3");
         playerShieldTextureRegion = textureAtlas.findRegion("shield2");
         enemyShieldTextureRegion = textureAtlas.findRegion("shield1");
-        enemyShieldTextureRegion.flip(false, true);
+        enemyShieldTextureRegion.flip(false, true);//dusman kalkani bize bakiyor
 
         playerLaserTextureRegion= textureAtlas.findRegion("laserBlue03");
         enemyLaserTextureRegion= textureAtlas.findRegion("laserRed03");
 
+        //set up game objects//yatay olarak ekranin ortasi ve dikey olarak 3. ceyregin bitisinde
         //set up game objects
-        playerShip = new Ship(2, 3, 10, 10,
-                WORLD_WIDTH/2, WORLD_HEIGHT/4,
-                playerShipTextureRegion, playerShieldTextureRegion);
-        enemyShip = new Ship(2, 1, 10, 10,
-                WORLD_WIDTH/2, WORLD_HEIGHT*3/4,
-                enemyShipTextureRegion, enemyShieldTextureRegion);
+        playerShip = new PlayerShip(WORLD_WIDTH / 2, WORLD_HEIGHT / 4,
+                10, 10,
+                2, 3,
+                0.4f, 4, 45, 0.5f,
+                playerShipTextureRegion, playerShieldTextureRegion, playerLaserTextureRegion);
+
+        enemyShip = new EnemyShip(WORLD_WIDTH / 2, WORLD_HEIGHT * 3 / 4,
+                10, 10,
+                2, 1,
+                0.3f, 5, 50, 0.8f,
+                enemyShipTextureRegion, enemyShieldTextureRegion ,enemyLaserTextureRegion);
+
+        playerLaserList = new LinkedList<>();
+        enemyLaserList = new LinkedList<>();
 
         batch = new SpriteBatch();//goruntuyu olusturur
     }
@@ -81,6 +97,9 @@ class GameScreen implements Screen {
     public void render(float deltaTime) {
 
         batch.begin();
+
+        playerShip.update(deltaTime);
+        enemyShip.update(deltaTime);
 
         //scrolling background
         renderBackground(deltaTime);
@@ -92,9 +111,43 @@ class GameScreen implements Screen {
         playerShip.draw(batch);
 
         //lasers
+        //create new lasers
+        //player lasers
+        if (playerShip.canFireLaser()) {
+            Laser[] lasers = playerShip.fireLasers();
+            for (Laser laser : lasers) {
+                playerLaserList.add(laser);
+            }
+        }
+        //enemy lasers
+        if (enemyShip.canFireLaser()) {
+            Laser[] lasers = enemyShip.fireLasers();
+            for (Laser laser : lasers) {
+                enemyLaserList.add(laser);
+            }
+        }
 
-        //explosions
+        //draw lasers
+        //remove old lasers
+        ListIterator<Laser> iterator = playerLaserList.listIterator();//liste yenileyici olusturuyoruz
+        while (iterator.hasNext()) {//yani sona gelmediyse
+            Laser laser = iterator.next();
+            laser.draw(batch);
+            laser.yPosition += laser.movementSpeed * deltaTime;//listeyi yenilerken  y pozisyonunu duzenliyor
+            if (laser.yPosition > WORLD_HEIGHT) {
+                iterator.remove();
+            }
+        }
+        iterator = enemyLaserList.listIterator();
+        while (iterator.hasNext()) {
+            Laser laser = iterator.next();
+            laser.draw(batch);
+            laser.yPosition -= laser.movementSpeed * deltaTime;
+            if (laser.yPosition + laser.height < 0) {
+                iterator.remove();
+            }
 
+        }
         batch.end();
     }
 
